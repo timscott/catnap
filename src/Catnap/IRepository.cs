79115e3ffccd5ff002907ee49403739909d2826c
tscott@lunaversesoftware.com <tscott@lunaversesoftware.com@810e4688-b3c2-11de-a8e9-03fe98c3e7a5>
2010-03-28 23:01:20 +0000

    Added generic repository implementation.  Used the generic repo as the inner repo for the abstract repo implementation.

--- src/Catnap/IRepository.cs
+++ src/Catnap/IRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Catnap.Find;
 using Catnap.Find.Conditions;
+using Catnap.Maps;
 
 namespace Catnap
 {
@@ -13,5 +15,15 @@ namespace Catnap
         IEnumerable<T> Find();
         IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
         IEnumerable<T> Find(ICriteria criteria);
+    }
+
+    public interface IRepository
+    {
+        T Get<T>(int id) where T : class, IEntity, new();
+        void Save<T>(T entity) where T : class, IEntity, new();
+        void Delete<T>(int id) where T : class, IEntity, new();
+        IEnumerable<T> Find<T>() where T : class, IEntity, new();
+        IEnumerable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : class, IEntity, new();
+        IEnumerable<T> Find<T>(ICriteria criteria) where T : class, IEntity, new();
     }
 }
--- src/Catnap/Repository.cs
+++ src/Catnap/Repository.cs
@@ -9,43 +9,45 @@ namespace Catnap
 {
     public abstract class Repository<T> : IRepository<T> where T : class, IEntity, new() 
     {
-        protected IEntityMap<T> entityMap;
+        protected readonly IRepository innerRepository;
+        protected readonly IEntityMap<T> entityMap;
 
-        protected Repository() : this(Domain.Map.GetMapFor<T>()) { }
+        protected Repository() : this(new Repository(), Domain.Map.GetMapFor<T>()) { }
 
-        protected Repository(IEntityMap<T> entityMap)
+        protected Repository(IRepository innerRepository, IEntityMap<T> entityMap)
         {
+            this.innerRepository = innerRepository;
             this.entityMap = entityMap;
         }
 
         public IEnumerable<T> Find()
         {
-            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().Build());
+            return innerRepository.Find<T>();
         }
 
         public IEnumerable<T> Find(ICriteria criteria)
         {
-            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().AddCriteria(criteria).Build());
+            return innerRepository.Find<T>(criteria);
         }
 
         public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
         {
-            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().AddCondition(predicate).Build());
+            return innerRepository.Find(predicate);
         }
 
         public virtual void Save(T entity)
         {
-            UnitOfWork.Current.Session.SaveOrUpdate(entity);
+            innerRepository.Save(entity);
         }
 
         public virtual T Get(int id)
         {
-            return UnitOfWork.Current.Session.Get<T>(id);
+            return innerRepository.Get<T>(id);
         }
 
         public virtual void Delete(int id)
         {
-            UnitOfWork.Current.Session.Delete<T>(id);
+            innerRepository.Delete<T>(id);
         }
 
         protected void DeleteCollection<TList>(IEnumerable<TList> collection, IEntityMap map)
@@ -53,7 +55,7 @@ namespace Catnap
         {
             foreach (var entity in collection)
             {
-                UnitOfWork.Current.Session.Delete<TList>(entity.Id);
+                innerRepository.Delete<T>(entity.Id);
             }
         }
 
@@ -64,6 +66,39 @@ namespace Catnap
             {
                 UnitOfWork.Current.Session.SaveOrUpdate(entity, parentId);
             }
+        }
+    }
+
+    public class Repository : IRepository
+    {
+        public T Get<T>(int id) where T : class, IEntity, new()
+        {
+            return UnitOfWork.Current.Session.Get<T>(id);
+        }
+
+        public void Save<T>(T entity) where T : class, IEntity, new()
+        {
+            UnitOfWork.Current.Session.SaveOrUpdate(entity);
+        }
+
+        public void Delete<T>(int id) where T : class, IEntity, new()
+        {
+            UnitOfWork.Current.Session.Delete<T>(id);
+        }
+
+        public IEnumerable<T> Find<T>() where T : class, IEntity, new()
+        {
+            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().Build());
+        }
+
+        public IEnumerable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : class, IEntity, new()
+        {
+            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().AddCondition(predicate).Build());
+        }
+
+        public IEnumerable<T> Find<T>(ICriteria criteria) where T : class, IEntity, new()
+        {
+            return UnitOfWork.Current.Session.List<T>(new FindCommandBuilder<T>().AddCriteria(criteria).Build());
         }
     }
 }
