b39b288ebbe4d76c8a0c1e76f4a5a132ec8d2cfc
Tim Scott <tscott@lunaversesoftware.com>
2012-01-05 10:33:48 -0600

    Removed obsolete repository stuff form tests.  Removed silly build.bat file.

--- src/Catnap.IntegrationTests/BasicTests.cs
+++ src/Catnap.IntegrationTests/BasicTests.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Catnap.Citeria.Conditions;
 using Catnap.Configuration;
 using Catnap.Database;
 using Catnap.IntegrationTests.Migrations;
-using Catnap.IntegrationTests.Repositories;
 using Catnap.Logging;
 using Catnap.Tests.Core;
 using Catnap.Tests.Core.Models;
@@ -39,10 +39,20 @@ namespace Catnap.IntegrationTests
         protected static void save_person()
         {
             person = new Person { FirstName = "Joe", LastName = "Smith" };
-            Container.PersonRepository.Save(person);
+            save_person(person);
         }
 
         Establish context = save_person;
+
+        protected static void save_person(Person person)
+        {
+            UnitOfWork.Current.Session.SaveOrUpdate(person);
+        }
+
+        protected static Person get_person(int id)
+        {
+            return UnitOfWork.Current.Session.Get<Person>(id);
+        }
     }
 
     public abstract class behaves_like_person_test_guids : behaves_like_integration_test
@@ -50,24 +60,36 @@ namespace Catnap.IntegrationTests
         protected static PersonGuid person;
         protected static PersonGuid actualPerson;
 
+        Establish context = save_person;
+
         protected static void save_person()
         {
             person = new PersonGuid { FirstName = "Joe", LastName = "Smith" };
-            ContainerGuid.PersonRepository.Save(person);
+            save_person(person);
         }
 
-        Establish context = save_person;
+        protected static void save_person(PersonGuid person)
+        {
+            UnitOfWork.Current.Session.SaveOrUpdate(person);
+        }
+
+        protected static PersonGuid get_person(Guid id)
+        {
+            return UnitOfWork.Current.Session.Get<PersonGuid>(id);
+        }
     }
 
     public class when_getting_person_ints : behaves_like_person_test_ints
     {
-        Because of = () => actualPerson = Container.PersonRepository.Get(person.Id);
+        Because of = () => actualPerson = get_person(person.Id);
+
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 
     public class when_getting_person_guids : behaves_like_person_test_guids
     {
-        Because of = () => actualPerson = ContainerGuid.PersonRepository.Get(person.Id);
+        Because of = () => actualPerson = get_person(person.Id);
+
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 
@@ -75,10 +97,10 @@ namespace Catnap.IntegrationTests
     {
         Because of = () =>
         {
-            actualPerson = Container.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.LastName = "Newlastname";
-            Container.PersonRepository.Save(actualPerson);
-            actualPerson = Container.PersonRepository.Get(person.Id);
+            save_person(actualPerson);
+            actualPerson = get_person(person.Id);
         };
         It should_have_updates = () => actualPerson.LastName.Should().Equal("Newlastname");
     }
@@ -87,30 +109,30 @@ namespace Catnap.IntegrationTests
     {
         Because of = () => 
         {
-            actualPerson = ContainerGuid.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.LastName = "Newlastname";
-            ContainerGuid.PersonRepository.Save(actualPerson);
-            actualPerson = ContainerGuid.PersonRepository.Get(person.Id);
+            save_person(actualPerson);
+            actualPerson = get_person(person.Id);
         };
         It should_have_updates = () => actualPerson.LastName.Should().Equal("Newlastname");
     }
 
     public class when_deleting_person_ints: behaves_like_person_test_ints
     {
-        Because of = () => Container.PersonRepository.Delete(person.Id);
+        Because of = () => UnitOfWork.Current.Session.Delete<Person>(person.Id);
         It person_should_be_deleted = () =>
         {
-            actualPerson = Container.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.Should().Be.Null();
         };
     }
 
     public class when_deleting_person_guids : behaves_like_person_test_guids
     {
-        Because of = () => ContainerGuid.PersonRepository.Delete(person.Id);
+        Because of = () => UnitOfWork.Current.Session.Delete<PersonGuid>(person.Id);
         It person_should_be_deleted = () =>
         {
-            actualPerson = ContainerGuid.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.Should().Be.Null();
         };
     }
@@ -118,10 +140,10 @@ namespace Catnap.IntegrationTests
     public class when_updating_a_person_ints : behaves_like_person_test_ints
     {
         Establish context =()=> person.FirstName = "NewFirstName";
-        Because of = () => Container.PersonRepository.Save(person);
+        Because of = () => save_person(person);
         It person_should_be_updated = () =>
         {
-            actualPerson = Container.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.FirstName.Should().Equal(person.FirstName);
         };
     }
@@ -129,23 +151,32 @@ namespace Catnap.IntegrationTests
     public class when_updating_a_person_guids : behaves_like_person_test_guids
     {
         Establish context = () => person.FirstName = "NewFirstName";
-        Because of = () => ContainerGuid.PersonRepository.Save(person);
+        Because of = () => save_person(person);
         It person_should_be_updated = () =>
         {
-            actualPerson = ContainerGuid.PersonRepository.Get(person.Id);
+            actualPerson = get_person(person.Id);
             actualPerson.FirstName.Should().Equal(person.FirstName);
         };
     }
 
     public class when_getting_person_by_first_name_ints : behaves_like_person_test_ints
     {
-        Because of = () => actualPerson = Container.PersonRepository.FindByFirstName(person.FirstName).FirstOrDefault();
+        Because of = () =>
+        {
+            var criteria = Criteria.For<Person>().Equal(x => x.FirstName, person.FirstName);
+            actualPerson = UnitOfWork.Current.Session.List(criteria).FirstOrDefault();
+        };
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 
     public class when_getting_person_by_first_name_guids : behaves_like_person_test_guids
     {
-        Because of = () => actualPerson = ContainerGuid.PersonRepository.FindByFirstName(person.FirstName).FirstOrDefault();
+        Because of = () =>
+        {
+            var criteria = Criteria.For<PersonGuid>()
+                .Equal(x => x.FirstName, person.FirstName);
+            actualPerson = UnitOfWork.Current.Session.List(criteria).FirstOrDefault();
+        };
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 
@@ -172,12 +203,12 @@ namespace Catnap.IntegrationTests
                 TimeOfDayLastUpdated = new TimeSpan(10, 9, 8, 7, 6),
                 Posts = new List<Post> {post}
             };
-            Container.ForumRepository.Save(forum);
+            UnitOfWork.Current.Session.SaveOrUpdate(forum);
         };
 
         protected static void get_forum()
         {
-            actualForum = Container.ForumRepository.Get(forum.Id);
+            actualForum = UnitOfWork.Current.Session.Get<Forum>(forum.Id);
         }
     }
 
@@ -204,12 +235,12 @@ namespace Catnap.IntegrationTests
                 TimeOfDayLastUpdated = new TimeSpan(10, 9, 8, 7, 6),
                 Posts = new List<PostGuid> {post}
             };
-            ContainerGuid.ForumRepository.Save(forum);
+            UnitOfWork.Current.Session.SaveOrUpdate(forum);
         };
 
         protected static void get_forum()
         {
-            actualForum = ContainerGuid.ForumRepository.Get(forum.Id);
+            actualForum = UnitOfWork.Current.Session.Get<ForumGuid>(forum.Id);
         }
     }
 
@@ -233,14 +264,20 @@ namespace Catnap.IntegrationTests
 
     public class when_getting_persons_who_have_posted_ints : behaves_like_post_test_ints
     {
-        static IList<Person> personsWhoHavePosted;
+        static IEnumerable<Person> personsWhoHavePosted;
 
         Establish context = () =>
         {
             var notPosted = new Person { FirstName = "HasNot", LastName = "Posted" };
-            Container.PersonRepository.Save(notPosted);
+            save_person(notPosted);
         };
-        Because of = () => personsWhoHavePosted = Container.PersonRepository.GetPesonsWhoHavePosted().ToList();
+
+        Because of = () =>
+        {
+            var command = new DbCommandSpec()
+                .SetCommandText("select p.* from Person p inner join Post on Post.PosterId = p.Id");
+            personsWhoHavePosted = UnitOfWork.Current.Session.List<Person>(command);
+        };
         It should_return_only_person_who_have_posted = () =>
         {
             personsWhoHavePosted.Should().Count.Exactly(1);
@@ -250,14 +287,19 @@ namespace Catnap.IntegrationTests
 
     public class when_getting_persons_who_have_posted_guids : behaves_like_post_test_guids
     {
-        static IList<PersonGuid> personsWhoHavePosted;
+        static IEnumerable<PersonGuid> personsWhoHavePosted;
 
         Establish context = () =>
         {
             var notPosted = new PersonGuid { FirstName = "HasNot", LastName = "Posted" };
-            ContainerGuid.PersonRepository.Save(notPosted);
+            save_person(notPosted);
         };
-        Because of = () => personsWhoHavePosted = ContainerGuid.PersonRepository.GetPesonsWhoHavePosted().ToList();
+        Because of = () =>
+        {
+            var command = new DbCommandSpec()
+                .SetCommandText("select p.* from PersonGuid p inner join PostGuid on PostGuid.PosterId = p.Id");
+            personsWhoHavePosted = UnitOfWork.Current.Session.List<PersonGuid>(command);
+        };
         It should_return_only_person_who_have_posted = () =>
         {
             personsWhoHavePosted.Should().Count.Exactly(1);
@@ -270,7 +312,13 @@ namespace Catnap.IntegrationTests
         static long postCount;
         private const long expected = 1;
 
-        Because of = () => postCount = Container.PersonRepository.GetTotalPostCount(person.Id);
+        Because of = () =>
+        {
+            var commandSpec = new DbCommandSpec()
+                .SetCommandText("select count(*) from Post p where p.PosterId = @personId")
+                .AddParameter("@personId", person.Id);
+            postCount = UnitOfWork.Current.Session.ExecuteScalar<long>(commandSpec);
+        };
         It should_return_post_count = () => postCount.Should().Equal(expected);
     }
 
@@ -279,7 +327,13 @@ namespace Catnap.IntegrationTests
         static long postCount;
         private const long expected = 1;
 
-        Because of = () => postCount = ContainerGuid.PersonRepository.GetTotalPostCount(person.Id);
+        Because of = () =>
+        {
+            var command = new DbCommandSpec()
+                .SetCommandText("select count(*) from PostGuid p where p.PosterId = @personId")
+                .AddParameter("@personId", person.Id);
+            postCount = UnitOfWork.Current.Session.ExecuteScalar<long>(command);
+        };
         It should_return_post_count = () => postCount.Should().Equal(expected);
     }
     
@@ -291,16 +345,24 @@ namespace Catnap.IntegrationTests
         Establish context = () =>
         {
             person = new Person { LastName = "Smith" };
-            Container.PersonRepository.Save(person);
+            UnitOfWork.Current.Session.SaveOrUpdate(person);
         };
 
-        Because of = () => actualPerson = Container.PersonRepository.FindWhereFirstNameNull().FirstOrDefault();
+        Because of = () =>
+        {
+            var criteria = Criteria.For<Person>().Null(x => x.FirstName);
+            actualPerson = UnitOfWork.Current.Session.List(criteria).FirstOrDefault();
+        };
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 
     public class when_getting_person_with_not_null_first_name : behaves_like_person_test_ints
     {
-        Because of = () => actualPerson = Container.PersonRepository.FindWhereFirstNameNotNull().FirstOrDefault();
+        Because of = () =>
+        {
+            var criteria = Criteria.For<Person>().NotNull("FirstName");
+            actualPerson = UnitOfWork.Current.Session.List(criteria).FirstOrDefault();
+        };
         It should_be_the_person = () => actualPerson.Should().Equal(person);
     }
 }
